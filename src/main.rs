202a6518f5808e981291bc9684c5d99179c77baa
vert2 <vert2air@gmail.com>
2021-01-09 02:27:19 +0900

    modify range to measure running time.

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,8 @@ fn main() {
     println!("Hello, world!");
     //let f = OurInt::try_from(1).unwrap();
     //let f = OurInt::try_from(20_310).unwrap();
-    let f = OurInt::try_from(2_471_476).unwrap();
-    let to = Some(OurInt::try_from(100).unwrap());
-    lazy_k_mining::mining(f, None);
+    let f = OurInt::try_from(2_471_450).unwrap();
+    //let to = Some(OurInt::try_from(100).unwrap());
+    let to = Some(OurInt::try_from(5_471_450).unwrap());
+    lazy_k_mining::mining(f, to);
 }
