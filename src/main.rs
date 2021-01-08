85088e66d0aa49c39bf5f85a260f18be4f3c0007
vert2 <vert2air@gmail.com>
2021-01-09 02:54:18 +0900

    modify to measure running time.

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,9 @@ use lazy_k::lazy_k_mining;
 
 fn main() {
     println!("Hello, world!");
-    let f = OurInt::try_from(1).unwrap();
-    let to = Some(OurInt::try_from(100).unwrap());
+    //let f = OurInt::try_from(1).unwrap();
+    let f = OurInt::try_from(2_471_450).unwrap();
+    //let to = Some(OurInt::try_from(100).unwrap());
+    let to = Some(OurInt::try_from(5_471_450).unwrap());
     lazy_k_mining::mining(f, to);
 }
