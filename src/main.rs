25bbca960121efdb159c88bae824f0b3b62cbcd8
vert2 <vert2air@gmail.com>
2021-01-18 02:29:05 +0900

    Compile passed.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,27 @@
 extern crate num_traits;
 use std::convert::TryFrom;
+use std::env;
 
 use lazy_k::lazy_k_core;
 use lazy_k::lazy_k_goedel_number;
 use lazy_k::lazy_k_goedel_number::OurInt;
 use lazy_k::lazy_k_mining;
 
+
 fn main() {
-    println!("Hello, world!");
-    //let f = OurInt::try_from(1).unwrap();
-    let f = OurInt::try_from(2_471_450).unwrap();
-    //let to = Some(OurInt::try_from(100).unwrap());
-    let to = Some(OurInt::try_from(5_471_450).unwrap());
-    lazy_k_mining::mining(f, to);
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() <= 1 {
+        match &args[1] {
+            x if x == "gn_to_unlam" && args.len() == 3 =>
+                lazy_k::gn_to_unlam(&args[2]),
+            x if x == "unlam_to_gn" && args.len() == 3 =>
+                lazy_k::unlam_to_gn(&args[2]),
+            x if x == "minging" =>
+                lazy_k::mining_between(args),
+            _ => println!("gn_to_unlam, unlam_to_gn, mining"),
+        }
+
+    }
 }
+
