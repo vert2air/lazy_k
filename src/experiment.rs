b47867795b6d48a75281c64b1d789f2ea0efea47
vert2 <vert2air@gmail.com>
2021-02-01 01:58:35 +0900

    Snapshot.
    Compile NG.

--- src/experiment.rs
+++ src/experiment.rs
@@ -8,6 +8,7 @@ use super::lazy_k_read;
 use super::lazy_k_core;
 use super::lazy_k_core::{PLamExpr, LamExpr};
 use super::goedel_number;
+use super::goedel_number::{OurInt, mul_up_down};
 
 pub fn experiment(args: Vec<String>) {
     let prog = fs::read_to_string(&args[2]).unwrap();
@@ -65,6 +66,79 @@ fn inc(map: &mut BTreeMap<BigInt, u32>, idx: BigInt) {
         None    => map.insert(idx, 1),
     };
 }
+
+struct LenToNum {
+    num: Vec<OurInt>,
+    acc: Vec<OurInt>,
+    base: OurInt,
+}
+
+impl LenToNum {
+
+    fn new(b: i32) -> Self {
+        LenToNum {
+            num: vec![],
+            acc: vec![],
+            base: b,
+        }
+    }
+
+    fn prepare_len(&self, len: usize) {
+        let idx = len - 1;
+        match self.num.get(idx) {
+            Some(_) => (),
+            None => match idx {
+                0 => {
+                    self.num.insert(0, self.base);
+                    self.acc.insert(0, self.base);
+                },
+                i => {
+                    self.prepare_len(i);
+                    let v = mul_up_down(self.num.iter().to_list());
+                    self.num.insert(i, v);
+                    self.acc.insert(i, v + self.acc.get(i).unwrap());
+                }
+            }
+        }
+    }
+
+    fn prepare_gn(&self, gn: OurInt) {
+        loop {
+            if gn < self.acc[self.acc.len() - 1] {
+                break;
+            }
+            self.prepare_len(self.acc.len() + 1)
+        }
+    }
+
+    /// Return the number of all expressions which has 'len' functions 
+    /// ```
+    /// fn n(num: u32) -> OurInt {
+    ///     match OurInt::try_from(num) {
+    ///         Ok(a) => a,
+    ///         _ => panic!("lam_to_n(0)"),
+    ///     }
+    /// };
+    /// 
+    /// let l2n = LenToNum::new(3);
+    /// assert_eq!( l2n.get(n(1)), n(3) )
+    /// assert_eq!( l2n.get(n(2)), n(3 * 3) )       // 9
+    /// //assert_eq!( l2n.get(n(3)), n(3 * 9 * 2) )   // 54
+    /// assert_eq!( l2n.get(n(4)), n(3 * 54 * 2 + 9 * 9) )
+    /// ```
+    fn get(&mut self, len: &usize) -> OurInt {
+        let idx = len - 1;
+        match self.num.get(idx) {
+            Some(n) => n,
+            None => {
+                self.prepare(len);
+                self.num.get(idx).unrwap()
+            }
+        }
+    }
+}
+
+
 
 
 /*
